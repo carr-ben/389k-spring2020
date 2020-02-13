080c1f8bb742bc6c32a8742af3b1d625ff63b664
Ben Carr <bencarr2323@gmail.com>
2020-02-12 23:34:14 -0500

    Completed p2

---
+++ lectures/week_02-javascript/inclassworksheet.js
@@ -0,0 +1,20 @@
+var usernames = ["cammie", "Chidi", "Benny", "Chirag"];
+var passwords = ["$56#hdj", "$56#asd", "$56#mqw", "$56#sla"];
+
+function createObjects(usernames, passwords) {
+     let objects = {};
+     for (var i in names) {
+          objects.usernames[i] = passwords[i];
+     }
+     return objects
+}
+
+var animals = {"rat":0, "Hamster":100, "cat":200, "dog":300, "rabbit":400, "lizard":500, "goldfish":600, "fly":700};
+
+function addLengthThree(list) {
+     let threeTotal = 0
+     let otherTotal = 0
+     for (var i of list) {
+
+     }
+}
